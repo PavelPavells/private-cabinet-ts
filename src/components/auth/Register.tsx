44320180b1892e214b387ce35ecb7aa49e20d8f9
PavelPavells <hismailforyou@gmail.com>
2020-05-08 15:49:18 +0300

    [08.05.2020 - Relocation Project on Typescript; Relocation Redux Store, Change Styles Login, Register Components; Pavel Smirnov]

--- src/components/auth/Register.tsx
+++ src/components/auth/Register.tsx
@@ -1,267 +1,296 @@
-/** ********** IMPORT LIBRARIES ********** */
+/**
+ * ********** Импорт зависимостей **********
+ */
 import React from "react";
 import { Link, withRouter } from "react-router-dom";
-import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { PersonalCabinet } from '../../store/store';
 
-/** ********** IMPORT ACTIONS ********** */
+/**
+ * ********** Импорт экшенов **********
+ */
 import { registerUser } from "../../actions/authActions";
 
-/** ********** IMPORT STYLES ********** */
+/**
+ * ********** Импорт стилей **********
+ */
 import "./Auth.scss";
 
-class Register extends React.PureComponent {
-  constructor() {
-    // @ts-ignore
-    super();
-    this.state = {
-      company_name: "",
-      company_inn: "",
-      name: "",
+/**
+ * ********** Интерфейс пропсов компонента Login **********
+ */
+interface LoginProps {
+  readonly registerUser: () => void,
+  readonly data: any,
+  readonly errors: any
+}
+
+/**
+ * ********** Интерфейс локального стейта компонента Login **********
+ */
+interface LoginState {
+  readonly login: string,
+  readonly pass: string,
+  readonly email: string,
+  readonly surname: string,
+  readonly name: string,
+  readonly patronymic: string,
+  readonly errors: any
+}
+
+class Register extends React.PureComponent<LoginProps, LoginState> {
+  
+    state: LoginState = {
+      login: "",
       pass: "",
       email: "",
-      contact_person: "",
-      company_phone: "",
+      surname: "",
+      name: "",
+      patronymic: " ",
       errors: {}
-    };
-  }
+    }
 
   /** ********** FETCH DATA ACCOUNT ********** */
-  componentDidMount() {
+  //public componentDidMount() {
     // @ts-ignore
-    if (this.props.auth.isAuthenticated) {
+  //  if (this.props.auth.isAuthenticated) {
       // @ts-ignore
-      this.props.history.push('/dashboard');
-    }
-  }
+  //    this.props.history.push('/dashboard');
+  //  }
+  //}
   // @ts-ignore
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    if (nextProps.errors) {
-      this.setState({ errors: nextProps.errors });
-    }
-  }
+  //public UNSAFE_componentWillReceiveProps(nextProps) {
+  //  if (nextProps.errors) {
+  //    this.setState({ errors: nextProps.errors });
+  //  }
+  //}
 
   /** ********** CHANGE DATA FROM INPUT ********** */
-  // @ts-ignore
-  onChange = event => {
+  private onChange = (event: { target: { id: any; value: any; }; }) => {
+    // @ts-ignore
     this.setState({ [event.target.id]: event.target.value });
   };
 
   /** ********** REGISTER NEW USER ********** */
-  // @ts-ignore
-  onSubmit = event => {
+  onSubmit = (event: { preventDefault: () => void; }) => {
     event.preventDefault();
     const newUser = {
-      // @ts-ignore
-      name: this.state.name,
-      // @ts-ignore
+      login: this.state.login,
+      pass: this.state.pass,
       email: this.state.email,
-      // @ts-ignore
-      company_name: this.state.company_name,
-      // @ts-ignore
-      company_inn: this.state.company_inn,
-      // @ts-ignore
-      contact_person: this.state.contact_person,
-      // @ts-ignore
-      company_phone: this.state.company_phone,
-      // @ts-ignore
-      pass: this.state.pass
+      surname: this.state.surname,
+      name: this.state.name,
+      patronymic: this.state.patronymic
     };
     // @ts-ignore
-    this.props.createNewUser(newUser, this.props.history);
+    this.props.registerUser(newUser, this.props.history);
   };
+
+  /**
+   * ********** Отправка формы кнопкой Enter **********
+   */
+  private onKeyPress = (event: { key: string; preventDefault: () => void; stopPropagation: () => void; }) => {
+    if(event.key === 'Enter') {
+      event.preventDefault();
+      event.stopPropagation();
+      // @ts-ignore
+      this.onSubmit()
+    }
+  }
   render() {
-    // @ts-ignore
     const { errors } = this.state;
     return (
-      <div className="wrapper">
-        <div className="header-logo">
-          <img src="../../img/carddex_logo.png" alt="" />
-        </div>
-        <div className="base-wrapper">
-          <div className="main-paragraph">
-            <h1>Личный кабинет Карддекс</h1>
-            <h3>Личный кабинет партнеров и дилеров Карддекс</h3>
-            <p>
-              Войдите или зарегистрируйтесь для получения всей доступной
-              информации по продуктам Карддекс, вашим заказам, скидкам и
-              предложениям.
-            </p>
-          </div>
-          <div className="wrapper-separator"></div>
-          <div className="wrapper-auth">
-            <div className="wrapper-auth__header">
-              <Link to="/" className="link-login">
-                Регистрация                                   {/** ВХОД */}
-              </Link>
-              {/*
-              <Link to="/register" className="link-register active">
-                Регистрация
-              </Link>
-              */}
+      <div className='auth'>
+          <div className='auth-left'>
+            <div className="logo">
+              <img src="../../img/carddex_logo.png" alt="" />
             </div>
-            <form className="auth-form" noValidate onSubmit={this.onSubmit}>
+            <h1 className='heading'>Авторизация</h1>
+            <div className="auth-forms">
+              <div className="auth-forms__header">
+                <Link to="/" className="link-login">
+                  Вход
+                </Link>
+                <Link to="/register" className="link-register active">
+                  Регистрация
+                </Link>
+              </div>
+
               <div className="auth-group">
                 <label>
-                  <div className="auth-label">Наименование компании</div>
+                  <div className="auth-label">Логин</div>
                   <input
                     onChange={this.onChange}
+                    onKeyDown={this.onKeyPress}
+                    value={this.state.login}
                     // @ts-ignore
-                    value={this.state.company_name}
-                    // @ts-ignore
-                    error={errors.company_name}
-                    id="company_name"
-                    type="company_name"
+                    error={errors.email}
+                    id="login"
+                    type="email"
                     className="auth-input"
                   />
-                  <div className="auth-error">{errors.company_name}</div>
+                  <div className="auth-error">
+                    {errors.email}
+                    {errors.emailnotfound}
+                  </div>
                 </label>
               </div>
               <div className="auth-group">
                 <label>
-                  <div className="auth-label">ИНН</div>
+                  <div className="auth-label">Пароль</div>
                   <input
                     onChange={this.onChange}
+                    onKeyDown={this.onKeyPress}
+                    value={this.state.pass}
                     // @ts-ignore
-                    value={this.state.company_inn}
-                    // @ts-ignore
-                    error={errors.company_inn}
-                    id="company_inn"
-                    type="company_name"
+                    error={errors.pass}
+                    id="pass"
+                    type="password"
                     className="auth-input"
                   />
-                  <div className="auth-error">{errors.company_inn}</div>
+                  <div className="auth-error">
+                    {/*{err}*/}
+                    {errors.passwordincorrect}
+                  </div>
                 </label>
               </div>
               <div className="auth-group">
-                <div className="bottom-group">
-                  <label>
-                    <div className="auth-label">Имя пользователя</div>
-                    <input
-                      onChange={this.onChange}
-                      // @ts-ignore
-                      value={this.state.name}
-                      // @ts-ignore
-                      error={errors.name}
-                      id="name"
-                      type="text"
-                      className="auth-input"
-                    />
-                    <div className="auth-error">{errors.name}</div>
-                  </label>
-                </div>
-                <div className="auth-group">
-                  <label>
-                    <div className="auth-label">Пароль</div>
-                    <input
-                      onChange={this.onChange}
-                      // @ts-ignore
-                      value={this.state.pass}
-                      // @ts-ignore
-                      error={errors.pass}
-                      id="pass"
-                      type="password"
-                      className="auth-input"
-                    />
-                    <div className="auth-error">{errors.pass}</div>
-                  </label>
-                </div>
-                <div className="auth-group">
-                  <label>
-                    <div className="auth-label">Email</div>
-                    <input
-                      onChange={this.onChange}
-                      // @ts-ignore
-                      value={this.state.email}
-                      // @ts-ignore
-                      error={errors.email}
-                      id="email"
-                      type="email"
-                      className="auth-input"
-                    />
-                    <div className="auth-error">{errors.email}</div>
-                  </label>
-                </div>
-                <div className="auth-group">
-                  <label>
-                    <div className="auth-label">Контактное лицо</div>
-                    <input
-                      onChange={this.onChange}
-                      // @ts-ignore
-                      value={this.state.contact_person}
-                      // @ts-ignore
-                      error={errors.contact_person}
-                      id="contact_person"
-                      type="contact_person"
-                      className="auth-input"
-                    />
-                  </label>
-                </div>
-                <div className="auth-group">
-                  <label>
-                    <div className="auth-label">Телефон</div>
-                    <input
-                      onChange={this.onChange}
-                      // @ts-ignore
-                      value={this.state.company_phone}
-                      // @ts-ignore
-                      error={errors.company_phone}
-                      id="company_phone"
-                      type="company_name"
-                      className="auth-input"
-                    />
-                  </label>
-                </div>
-                <div>
-                  <div className="auth-password__ask">
-                    <label>
-                      <input type="checkbox" />
-                      Запомнить меня
-                    </label>
-                    <a
-                      href="https://yandex.ru"
-                      className="auth-group__ask-password"
-                    >
-                      Забыли пароль?
-                    </a>
+                <label>
+                  <div className="auth-label">Подтветдите пароль</div>
+                  <input
+                    onChange={this.onChange}
+                    onKeyDown={this.onKeyPress}
+                    value={this.state.pass}
+                    // @ts-ignore
+                    error={errors.pass}
+                    id="pass"
+                    type="password"
+                    className="auth-input"
+                  />
+                  <div className="auth-error">
+                    {/*{err}*/}
+                    {errors.passwordincorrect}
                   </div>
-                  <div className="auth-condition">
-                    <span>Нажимая кнопку "Войти", вы принимаете</span>
-                    <a
-                      href="https://yandex.ru"
-                      className="auth-conditon__confidiency"
-                    >
-                      Условия "Политики Конфиденциальности"
-                    </a>
+                </label>
+              </div>
+              <div className="auth-group">
+                <label>
+                  <div className="auth-label">Email</div>
+                  <input
+                    onChange={this.onChange}
+                    onKeyDown={this.onKeyPress}
+                    value={this.state.email}
+                    // @ts-ignore
+                    error={errors.pass}
+                    id="email"
+                    type="text"
+                    className="auth-input"
+                  />
+                  <div className="auth-error">
+                    {/*{err}*/}
+                    {errors.passwordincorrect}
                   </div>
-                  <button type="submit" className="auth-button">
-                    Зарегестрироваться
+                </label>
+              </div>
+              <div className="auth-group">
+                <label>
+                  <div className="auth-label">Фамилия</div>
+                  <input
+                    onChange={this.onChange}
+                    onKeyDown={this.onKeyPress}
+                    value={this.state.surname}
+                    // @ts-ignore
+                    error={errors.pass}
+                    id="surname"
+                    type="text"
+                    className="auth-input"
+                  />
+                  <div className="auth-error">
+                    {/*{err}*/}
+                    {errors.passwordincorrect}
+                  </div>
+                </label>
+              </div>
+              <div className="auth-group">
+                <label>
+                  <div className="auth-label">Имя</div>
+                  <input
+                    onChange={this.onChange}
+                    onKeyDown={this.onKeyPress}
+                    value={this.state.name}
+                    // @ts-ignore
+                    error={errors.pass}
+                    id="name"
+                    type="text"
+                    className="auth-input"
+                  />
+                  <div className="auth-error">
+                    {/*{err}*/}
+                    {errors.passwordincorrect}
+                  </div>
+                </label>
+              </div>
+              <div className="auth-group">
+                <label>
+                  <div className="auth-label">Отчетсво</div>
+                  <input
+                    onChange={this.onChange}
+                    onKeyDown={this.onKeyPress}
+                    value={this.state.patronymic}
+                    // @ts-ignore
+                    error={errors.pass}
+                    id="patronymic"
+                    type="text"
+                    className="auth-input"
+                  />
+                  <div className="auth-error">
+                    {/*{err}*/}
+                    {errors.passwordincorrect}
+                  </div>
+                </label>
+              </div>
+              <div className="auth-password__ask">
+                <label>
+                  <input type="checkbox" /> Запомнить меня
+                </label>
+                <a
+                  href="https://yandex.ru"
+                  className="auth-group__ask-password"
+                >
+                  Забыли пароль?
+                </a>
+              </div>
+              <div className="auth-condition">
+                <span>Нажимая кнопку "Войти", вы принимаете</span>
+                <a
+                  href="https://yandex.ru"
+                  className="auth-conditon__confidiency"
+                >
+                  Условия "Политики Конфиденциальности"
+                </a>
+              </div>
+              <div>
+                <Link to="/">
+                  <button
+                    onClick={this.onSubmit}
+                    type="submit"
+                    className="auth-button"
+                  >
+                    Регистрация
                   </button>
-                </div>
+                </Link>
               </div>
-            </form>
+            </div>
+          </div>
+          <div className='auth-right'>
+            <div className='image' />
           </div>
         </div>
-        <div className="footer">
-          <div className="footer-copy">
-            <h1>Copyright 2019 &copy; CARDDEX</h1>
-            <p>Информация на сайте не является публичной офертой</p>
-          </div>
-        </div>
-      </div>
     );
   }
 }
 
-// @ts-ignore
-Register.propTypes = {
-  registerUser: PropTypes.func,
-  auth: PropTypes.object,
-  errors: PropTypes.object.isRequired
-};
-
-// @ts-ignore
-const mapStateToProps = state => ({
+const mapStateToProps = (state: PersonalCabinet) => ({
   auth: state.auth,
   errors: state.errors
 });
